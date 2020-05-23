91589bdbe1db9465dfba745257630454a3a67764
Heegreis <run3113214@gmail.com>
2020-05-23 22:33:50 +0800

    change layout and add d3.js

---
+++ src/pages/Map.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page>
+    <p> Hello, D3.js. </p>
+  </q-page>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import * as d3 from 'd3'
+
+export default Vue.extend({
+  name: 'Map',
+  mounted: function () {
+    d3.select('p').style('color', 'red')
+  }
+})
+</script>
