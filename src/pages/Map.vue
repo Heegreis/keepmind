b835807ea51477cea01e49296ef9f564fec9f72b
Heegreis <run3113214@gmail.com>
2020-05-24 12:04:55 +0800

    set eslint conform fomatter

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -1,17 +1,28 @@
 <template>
   <q-page>
-    <p> Hello, D3.js. </p>
+    <p>Hello, D3.js.</p>
+    <svg
+      width="200"
+      height="200"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        r="50"
+        style="fill:red"
+      ></circle>
+    </svg>
   </q-page>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import * as d3 from 'd3'
+import Vue from "vue";
+import * as d3 from "d3";
 
 export default Vue.extend({
-  name: 'Map',
-  mounted: function () {
-    d3.select('p').style('color', 'red')
+  name: "Map",
+  mounted: function() {
+    d3.select("p").style("color", "red");
   }
-})
+});
 </script>
